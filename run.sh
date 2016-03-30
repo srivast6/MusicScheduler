c401f5a5b7dfa9f76c851658894581e8582a6dc7
Mootix1313 <rachellehcar.gully1@gmail.com>
2016-03-29 22:57:31 -0400

    Updated the run.sh to create and run a jar version of the program

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@ make clean
 make
 
 #Execute the program
-java -cp .:./lib/jl1.0.1.jar:./classes MusicHome
+#java -cp .:./lib/jl1.0.1.jar:./classes MusicHome
+
+#Create a jar of the file
+jar -cfve MusicScheduler.jar MusicHome -C classes . -C lib .
+
+#run the jar
+java -jar MusicScheduler.jar
